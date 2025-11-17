eec8832b65abdb93a532856cb16462d31ba572a8
Alex Javier Mendoza <mendozarossi0930@icloud.com>
2025-11-17 12:00:42 -0800

    Listas<>-

--- 10. Colecciones Genericas o de Objetos/Coleccion Genericas/6. Metodo List/Program.cs
+++ 10. Colecciones Genericas o de Objetos/Coleccion Genericas/6. Metodo List/Program.cs
@@ -20,38 +20,77 @@ internal class Program
 
         // Para agregar elementos se usa el metodo "add"
 
-            //personas.Add("Alex");
-            //personas.Add("Madeleine");
-            //personas.Add("Mariana");
-            //personas.Add("Elvira");
+        //personas.Add("Alex");
+        //personas.Add("Madeleine");
+        //personas.Add("Mariana");
+        //personas.Add("Elvira");
 
-        do
-        {
-            Console.WriteLine("1. Agregar elemento");
-            Console.WriteLine("2. Salir");
-            opcion = int.Parse(Console.ReadLine());
+        /*do
+         {
+             Console.WriteLine("1. Agregar elemento");
+             Console.WriteLine("2. Salir");
+             opcion = int.Parse(Console.ReadLine());
 
-            if(opcion == 1)
-            {
-                Console.WriteLine("Ingrese el elemento a agregar en la lista");
-                elemento = Console.ReadLine();
+             if(opcion == 1)
+             {
+                 Console.WriteLine("Ingrese el elemento a agregar en la lista");
+                 elemento = Console.ReadLine();
 
-                personas.Add(elemento);
-            }
-            Console.WriteLine("");
-        }
-        while (opcion == 1);
+                 personas.Add(elemento);
+             }
+             Console.WriteLine("");
+         }
+         while (opcion == 1); */
 
         //Mostramos los elementos de la lista:
         //for (int i = 0; i < personas.Count(); i++) ;
-       // {
-            //Console.WriteLine(personas[i]);
+        // {
+        //Console.WriteLine(personas[i]);
         //}
 
+        Console.WriteLine("Elementos de la lista:\n");
+        //Agregando Metodos:
+        personas.Add("Hugo");
+        personas.Add("Paco");
+        personas.Add("Luis");
+        personas.Add("Ana");
+
         //utilizandi foreach
-         foreach(string elementos in personas)
+        foreach (string elementos in personas)
         {
             Console.WriteLine(elementos);
         }
+
+        /*//Insertando un elemnto en la List
+        personas.Insert(2, "Carla");
+
+        Console.WriteLine("\nDespues de Insertar:\n");
+        foreach (string elementos in personas)
+        {
+            Console.WriteLine(elementos);
+        }*/
+
+        //---------------------------------------
+        //-                                     -
+        //-         IndexOf y Clear             -
+        //-                                     -
+        //---------------------------------------
+
+         //Console.WriteLine("\nIngresa el elemento a buscar");
+          //elemento = Console.ReadLine();
+
+        //buscando el elemento
+            //Console.WriteLine("El elemento se ubica en el indice {0}", personas.IndexOf(elemento));
+
+        personas.Clear();
+
+        Console.WriteLine("\nDespues de Clear:\n");
+        foreach (string elementos in personas)
+        {
+            Console.WriteLine(elementos);
+        }
+
+        Console.ReadKey();
+
     }
 }
---
+++ 10. Colecciones Genericas o de Objetos/Coleccion Genericas/7.Ejercicio Lista/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Ejercicio_Lista
+{
+    private static void Main(string[] args)
+    {
+        //Variables:
+        int opcion, indice;
+        string alumno;
+
+        //Instanciando la lista:
+        List<string> Alumnos = new List<string>();
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("1. Agregar Estudiante");
+            Console.WriteLine("2. Eliminar Estudiante");
+            Console.WriteLine("3. Mostrar Estudiante");
+            Console.WriteLine("4. Buscar por nombre");
+
+            //Escoger opcion:
+            Console.WriteLine("Seleccione una opcion:");
+            opcion = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            switch (opcion)
+            {
+                case 1:
+                    Console.Write("Ingresa el nombre del alumno: ");
+                    Alumnos.Add(alumno);
+                    break;
+                case 2:
+                    Console.Write("Ingresa el numero del estudiante que quieres eliminar: ");
+                    indice = int.Parse(Console.ReadLine());
+                    indice--; //El usuario no sabe que el indice empieza en 0.
+                    if (indice >= Alumnos.Count() || indice < 0)
+                    {
+                        Console.WriteLine("ALUMNO NO EXISTE");
+                    }
+                    else
+                    {
+                        string alumnoEliminado = Alumnos[indice];
+                        Alumnos.RemoveAt(indice);
+                        Console.WriteLine("ALUMNO ELIMINADO: ", alumnoEliminado);
+                    }
+                    Console.WriteLine("\nPresiona una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+                case 3:
+                    int i = 1;
+
+                    foreach (string estudiante in Alumnos)
+                    {
+                        Console.WriteLine("{0}. {1}", i++, estudiante);
+                    }
+                    Console.WriteLine("\nPresiona una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+                case 4:
+                    string encontrarAlumno; int j;
+                    Console.Write("Ingresa el nombre del alumno a buscar: ");
+                    alumno = Console.ReadLine();
+                    //Verificar si alumno esta en la lista:
+                    if (Alumnos.IndexOf(alumno) >= 0)
+                    {
+                        encontrarAlumno = Alumnos[Alumnos.IndexOf(alumno)];
+                        j = Alumnos.IndexOf(alumno) + 1;
+                        Console.WriteLine("El estudiante {0} se encuentra en el numero {1} de la lista", encontrarAlumno, j);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El estudiante {0} no se encuentra en la lista", encontrarAlumno);
+                    }
+                    Console.WriteLine("\nPresiona una tecla para continuar...");
+                    Console.ReadKey();
+                    break;
+            }
+
+
+
+
+
+        };        while (opcion != 5) ;
+
+
+
+
+
+
+
+
